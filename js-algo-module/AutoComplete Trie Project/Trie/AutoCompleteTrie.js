da15090e86195b821bba1e3f0f09388748018b4d
Tzuriel Yeffet <tzuriel66ye@yahoo.com>
2025-07-22 16:48:16 +0300

    initial skeleton

---
+++ js-algo-module/AutoComplete Trie Project/Trie/AutoCompleteTrie.js
@@ -0,0 +1,26 @@
+class AutoCompleteTrie{
+    constructor(value){
+        this. value = value;
+        this.children = {};
+        this.endOfWord = false;
+    }
+
+    addWord(word){
+      
+    }
+    findWord(){
+
+    }
+
+    predictWords(prefix){
+
+    }
+    _getRemainingTree(prefix, node){
+
+    }
+    _allWordsHelper(prefix, node, allWords){
+
+    }
+}
+
+module.exports = AutoCompleteTrie;
