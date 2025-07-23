dc1c0b55be6f232106c7a63b2343c0e16fccb34d
Tzuriel Yeffet <tzuriel66ye@yahoo.com>
2025-07-23 17:29:31 +0300

    add unit test from AI

--- js-algo-module/AutoComplete Trie Project/Trie/AutoCompleteTrie.js
+++ js-algo-module/AutoComplete Trie Project/Trie/AutoCompleteTrie.js
@@ -28,17 +28,22 @@ class AutoCompleteTrie {
 
   predictWords(prefix) {
     let node = this;
-    for (let ch of prefix) {
-      if (!node.children[ch]) {
-        return [];
-      }
-      node = node.children[ch];
-    }
+    node = this._getRemainingTree(prefix, this);
+    if (!node) return [];
+
     let predictArr = [];
     node._allWordsHelper(prefix, node, predictArr);
     return predictArr;
   }
-  _getRemainingTree(prefix, node) {}
+  _getRemainingTree(prefix, node) {
+    for (let ch of prefix) {
+      if (!node.children[ch]) {
+        return null;
+      }
+      node = node.children[ch];
+    }
+    return node;
+  }
   _allWordsHelper(prefix, node, allWords) {
     if (node.endOfWord) {
       allWords.push(prefix);
--- js-algo-module/AutoComplete Trie Project/Trie/AutoCompleteTrie.test.js
+++ js-algo-module/AutoComplete Trie Project/Trie/AutoCompleteTrie.test.js
@@ -1,5 +1,14 @@
 const AutoCompleteTrie = require("./AutoCompleteTrie")
 
+
+describe("AutoCompleteTrie", () => {
+  let trie;
+
+  beforeEach(() => {
+    trie = new AutoCompleteTrie();
+  });
+
+  
 test("addNode method should add a new node to the trie",()=>{
     let trie = new AutoCompleteTrie();
     let value = "cat"
@@ -24,3 +33,35 @@ test("predictWords should return an array of all possible completions if exists,
     expect(trie.predictWords('ca')).toEqual(['cat',"car","can"]);
     expect(trie.predictWords('br')).toEqual([]);
 })
+
+  test("findWord should return false for a prefix only", () => {
+    trie.addWord("cat");
+    expect(trie.findWord("ca")).toBe(false);
+  });
+
+  test("findWord should return false for a completely unknown word", () => {
+    trie.addWord("cat");
+    expect(trie.findWord("dog")).toBe(false);
+  });
+
+  test("predictWords should return all completions for a given prefix", () => {
+    trie.addWord("cat");
+    trie.addWord("car");
+    trie.addWord("can");
+    trie.addWord("dog");
+
+    const result = trie.predictWords("ca").sort(); 
+    expect(result).toEqual(["can", "car", "cat"].sort());
+  });
+
+  test("predictWords should return an empty array for unknown prefix", () => {
+    trie.addWord("cat");
+    trie.addWord("dog");
+    expect(trie.predictWords("br")).toEqual([]);
+  });
+
+  test("predictWords should return full words for full match prefix", () => {
+    trie.addWord("cat");
+    expect(trie.predictWords("cat")).toEqual(["cat"]);
+  });
+});
