4a9b73acd4417cd82e0efabd303fcdb6816638d5
TzurielYeffet <70668027+TzurielYeffet@users.noreply.github.com>
2025-07-30 00:32:44 +0300

    Update Ex4.js

--- AJAX-and-API's/Ex4/Ex4.js
+++ AJAX-and-API's/Ex4/Ex4.js
@@ -1,5 +1,5 @@
 //Ex4
-const api_key ="amxAoqHIkFl406jWQ4wUcRmbA145quC7"
+const api_key =""
 const url =`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=cheeseburgers&limit=1`
 
 function fetchGIF() {
